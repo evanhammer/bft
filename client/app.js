207faa24747b8ecfe2060368412ecf1da9ead7c7
Evan Hammer <evan@evanhammer.com>
2014-08-09 15:47:44 -0500

    Count Up the time.

--- client/app.js
+++ client/app.js
@@ -1,13 +1,45 @@
-/* global Meteor, Template */
+/* global $, Template, moment */
 //Template.hello.greeting = function () {
 //    return "It's just a Big Fucking Timer.";
 //};
 
+var startTime = null;
+var intervalID = null;
+
+Template.bft.rendered = function () {
+    //$('.title').fitText(0.9);
+    $('.timer').fitText(0.48);
+};
+
 Template.bft.events({
-    'click input': function () {
-        // template data, if any, is available in 'this'
-        if (typeof console !== 'undefined') {
-            console.log("You pressed the button");
+    'click .count-up': function () {
+        if (intervalID) {
+            clearInterval(intervalID);
         }
+        startTime = moment();
+        intervalID = setInterval(function () {
+            var timeSince = moment.duration((moment().diff(startTime)));
+            var seconds = timeSince.seconds();
+            var minutes = timeSince.minutes();
+            var hours = Math.floor(timeSince.asHours());
+
+            var secondsStr = seconds.toString();
+            if (seconds < 10) {
+                secondsStr = "0" + secondsStr;
+            }
+
+            var minutesStr = minutes.toString();
+            if (minutes < 10) {
+                minutesStr = "0" + minutesStr;
+            }
+
+            var hoursStr = hours.toString();
+            if (hours < 10) {
+                hoursStr = "0" + hoursStr;
+            }
+            var time = hoursStr + ":" + minutesStr + ":" + secondsStr;
+            console.log(time);
+            $('#time').text(time);
+        }, 100);
     }
 });
