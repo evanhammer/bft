b155e6af540f0e9c6e3cdbd2e3b2bce1765d5ee8
Evan Hammer <evan@evanhammer.com>
2014-08-09 12:32:20 -0500

    Add meteor and meteorite.

---
+++ client/app.js
@@ -0,0 +1,13 @@
+/* global Meteor, Template */
+Template.hello.greeting = function () {
+    return "It's just a Big Fucking Timer.";
+};
+
+Template.hello.events({
+    'click input': function () {
+        // template data, if any, is available in 'this'
+        if (typeof console !== 'undefined') {
+            console.log("You pressed the button");
+        }
+    }
+});
---
+++ server/app.js
@@ -0,0 +1,4 @@
+/* global Meteor, Template */
+Meteor.startup(function () {
+    // code to run on server at startup
+});
